747dfbf55165ae3f73502e7037f13b8c5152e5ec
EzequielSR <ezequieldesr@gmail.com>
2024-12-05 18:23:46 -0300

    build: mediaQuery.css
    Adição de responsividade e organização de pastas

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+// Seleciona o botão de menu e os menus
+const menuHamburguer = document.getElementById("menu-hamburguer")!;
+const linksNavegacao = document.getElementById("links-navegacao")!;
+const redesSociais = document.getElementById("redes-sociais")!;
+
+// Função para alternar o menu
+const toggleMenu = () => {
+    const isMenuOpen = linksNavegacao.classList.contains("menu-fechado");
+    if (isMenuOpen) {
+        linksNavegacao.classList.remove("menu-fechado");
+        redesSociais.classList.remove("menu-fechado");
+    } else {
+        linksNavegacao.classList.add("menu-fechado");
+        redesSociais.classList.add("menu-fechado");
+    }
+};
+
+// Adiciona o evento de clique ao botão
+menuHamburguer.addEventListener("click", toggleMenu);
