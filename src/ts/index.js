1ca99cf7a0d4a4e748d2bf64a91e255aaabc2576
EzequielSR <ezequieldesr@gmail.com>
2025-01-23 11:35:19 -0300

    Remoção arquivos

--- src/ts/index.js
+++
@@ -1,8 +0,0 @@
-var menuToggle = document.getElementById('menu-toggle');
-var linksNavegacao = document.getElementById('links-navegacao');
-if (menuToggle && linksNavegacao) {
-    menuToggle.addEventListener('click', function () {
-        linksNavegacao.classList.toggle('active');
-        menuToggle.classList.toggle('active');
-    });
-}
