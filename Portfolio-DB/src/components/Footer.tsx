54aa965703d38f917664093e8d10be525d9e8cbf
EzequielSR <ezequieldesr@gmail.com>
2025-01-23 12:05:07 -0300

    -a

--- Portfolio-DB/src/components/Footer.tsx
+++ Portfolio-DB/src/components/Footer.tsx
@@ -1,12 +1,11 @@
 import "../css/styles.css";
 import "../css/mediaQuery.css";
-import logoFooter from "../../public/assets/d+b%20dark%20blue%20-%20horizontal%20(1)%201.png";
 
 const Footer = () => (
     <footer>
         <div className="footer-container">
             <img
-                src={logoFooter}
+                src="/assets/d+b%20dark%20blue%20-%20horizontal%20(1)%201.png"
                 alt="Logo do rodapé"
             />
         </div>
