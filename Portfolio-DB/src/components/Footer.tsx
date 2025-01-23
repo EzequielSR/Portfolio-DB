2a47e15275c85c1026ec7b5ae3b9b6ad6588ec0d
EzequielSR <ezequieldesr@gmail.com>
2025-01-23 11:48:44 -0300

    -a

--- Portfolio-DB/src/components/Footer.tsx
+++ Portfolio-DB/src/components/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => (
     <footer>
         <div className="footer-container">
             <img
-                src="/assets/d+b dark blue - horizontal (1) 1.png"
+                src="/assets/d+b%20dark%20blue%20-%20horizontal%20(1)%201.png"
                 alt="Logo do rodapé"
             />
         </div>
