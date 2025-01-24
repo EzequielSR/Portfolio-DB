24e227e1f5b107ff7d04b346bf6b298319a5f0fe
EzequielSR <ezequieldesr@gmail.com>
2025-01-24 10:55:48 -0300

    Remoção pasta src fora do projeto && adição do Header.tsx e Footer.tsx em App.tsx

--- Portfolio-DB/src/pages/Projetos.tsx
+++ Portfolio-DB/src/pages/Projetos.tsx
@@ -1,13 +1,9 @@
-import {Header} from "../components/Header.tsx";
-import Footer from "../components/Footer";
 import ProjetosCard from "../components/ProjetosCard";
 
 const Projetos: React.FC = () => {
     return (
         <>
-            <Header />
             <ProjetosCard/>
-            <Footer />
         </>
     )
 }
--- src/ts/index.ts
+++
@@ -1,9 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle') as HTMLButtonElement;
-const linksNavegacao = document.getElementById('links-navegacao') as HTMLElement;
-
-if (menuToggle && linksNavegacao) {
-    menuToggle.addEventListener('click', () => {
-        linksNavegacao.classList.toggle('active');
-        menuToggle.classList.toggle('active');
-    });
-}
