248504f8d362d5f5da00bda6609f50f0e487ee2a
EzequielSR <ezequieldesr@gmail.com>
2025-01-24 17:53:49 -0300

    Refatoração do arquivo ProjetosCard.tsx para os cards && da imagem ODesafioSimon.png

--- Portfolio-DB/src/components/ProjetosCard.tsx
+++ Portfolio-DB/src/components/ProjetosCard.tsx
@@ -1,7 +1,73 @@
 import "../css/styles.css";
 import "../css/mediaQuery.css";
+import {useEffect, useState} from "react";
 
 const ProjetosCard: React.FC = () => {
+    const [isTelaGrande, setTelaGrande] = useState(window.innerWidth > 1024);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setTelaGrande(window.innerWidth > 1024);
+        }
+
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+        }
+    }, []);
+    const projetos = [
+        {
+            nome: "EclipsaGarage",
+            tecnologias: "HTML, CSS",
+            descricao: "O EclipsaGarage é um portal de serviços automotivos simples, onde a paixão pelos carros encontra a facilidade de navegação, destacando os serviços e informações de contato para uma experiência intuitiva.",
+            imgSrc: "/assets/EclipsaGarage.png",
+            links: {
+                site: "https://eclipsa-garage.vercel.app/",
+                github: "https://github.com/EzequielSR/EclipsaGarage"
+            }
+        },
+        {
+            nome: "BoraJogar",
+            tecnologias: "HTML, CSS, JS, Bootstrap",
+            descricao: "BoraJogar é uma landing page criada para promover uma plataforma moderna de jogos online no navegador, inspirada no estilo clássico dos jogos Friv, mas com o design e funcionalidades mais próximas da experiência do Steam.",
+            imgSrc: "/assets/BoraJogar.png",
+            links: {
+                site: "https://bora-jogar.vercel.app/",
+                github: "https://github.com/EzequielSR/BoraJogar"
+            }
+        },
+        {
+            nome: "O Desafio Simon",
+            tecnologias: "HTML, CSS, JQuery",
+            descricao: "O Desafio Simon é uma versão web inspirada no clássico jogo eletrônico Simon Game, que desafia a memória dos jogadores por meio de sequências de luzes e sons. O objetivo do jogo é reproduzir corretamente as sequências que vão ficando progressivamente mais longas e complexas.",
+            imgSrc: "/assets/ODesafioSimon.png",
+            links: {
+                site: "https://o-desafio-simon.vercel.app/",
+                github: "https://github.com/EzequielSR/O_Desafio_Simon"
+            }
+        },
+        {
+            nome: "Produto API - Testes de Integração",
+            tecnologias: "JAVA, SpringBoot, JUnit, MockMvc",
+            descricao: "Produto API - Testes de Integração é uma aplicação que valida os endpoints de uma API de gerenciamento de produtos, utilizando Spring Boot, JUnit 5 e MockMvc para garantir o funcionamento correto das funcionalidades, como criação e listagem de produtos, através de testes automatizados.",
+            imgSrc: "/assets/Produto-API.png",
+            links: {
+                github: "https://github.com/EzequielSR/Produto-API_Testes_Integracao"
+            }
+        },
+        {
+            nome: "IP Address Tracker",
+            tecnologias: "React, JavaScript, Node.js, Express, LeafletJS, Axios",
+            descricao: "IP Address Tracker é uma aplicação web que permite a pesquisa e visualização de informações de IP e domínios, mostrando dados como localização, fuso horário e ISP, além de exibir as coordenadas em um mapa interativo. Utiliza React no frontend, Node.js e Express no backend, com integração à API IPify.",
+            imgSrc: "/assets/ip-address-tracker.png",
+            links: {
+                site: "https://ip-address-tracker-master-virid-nine.vercel.app/",
+                github: "https://github.com/EzequielSR/ip-address-tracker-master"
+            }
+        }
+    ];
+
     return (
         <main>
             <section>
@@ -10,102 +76,34 @@ const ProjetosCard: React.FC = () => {
                     <h1>Projetos</h1>
                 </div>
                 <div className="card-container">
-                    <div className="card">
-                        <img src="/assets/EclipsaGarage.png" alt="Descrição da imagem"/>
-                        <div className="card-about">
-                            <h2>EclipsaGarage</h2>
-                            <span> HTML, CSS</span>
-                            <p>O EclipsaGarage é um portal de serviços automotivos simples, onde a paixão pelos carros
-                                encontra
-                                a facilidade de navegação, destacando os serviços e informações de contato para uma
-                                experiência
-                                intuitiva.</p>
-                            <div className="buttons">
-                                <button><a href="https://eclipsa-garage.vercel.app/" target="_blank"
-                                           rel="noopener noreferrer">Acessar</a></button>
-                                <button><a href="https://github.com/EzequielSR/EclipsaGarage" target="_blank"
-                                           rel="noopener noreferrer">Github</a></button>
+                    {projetos.map((projeto, index) => (
+                        <div className="card" key={index}>
+                            <img src={projeto.imgSrc} alt={`Descrição da imagem ${projeto.nome}`} />
+                            <div className="card-about">
+                                <h2>{projeto.nome}</h2>
+                                <span>{projeto.tecnologias}</span>
+                                <p title={isTelaGrande ? projeto.descricao : undefined}>
+                                    {projeto.descricao}
+                                </p>
+                                <div className="buttons">
+                                    {projeto.links.site && (
+                                        <button>
+                                            <a href={projeto.links.site} target="_blank" rel="noopener noreferrer">Acessar</a>
+                                        </button>
+                                    )}
+                                    {projeto.links.github && (
+                                        <button>
+                                            <a href={projeto.links.github} target="_blank" rel="noopener noreferrer">Github</a>
+                                        </button>
+                                    )}
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="card">
-                        <img src="/assets/BoraJogar.png" alt="Descrição da imagem"/>
-                        <div className="card-about">
-                            <h2>BoraJogar</h2>
-                            <span> HTML, CSS, JS, Bootstrap</span>
-                            <p>BoraJogar é uma landing page criada para promover uma plataforma moderna de jogos online
-                                no
-                                navegador, inspirada no estilo clássico dos jogos Friv, mas com o design e
-                                funcionalidades mais
-                                próximas da experiência do Steam.</p>
-                            <div className="buttons">
-                                <button><a href="https://bora-jogar.vercel.app/" target="_blank">Acessar</a></button>
-                                <button><a href="https://github.com/EzequielSR/BoraJogar" target="_blank">Github</a>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <img src="/assets/ODesafioSimon.png" alt="Descrição da imagem"/>
-                        <div className="card-about">
-                            <h2>O Desafio Simon</h2>
-                            <span> HTML, CSS, JQuery</span>
-                            <p>O Desafio Simon é uma versão web inspirada no clássico jogo eletrônico Simon Game, que
-                                desafia a
-                                memória dos jogadores por meio de sequências de luzes e sons. O objetivo do jogo é
-                                reproduzir
-                                corretamente as sequências que vão ficando progressivamente mais longas e complexas.</p>
-                            <div className="buttons">
-                                <button><a href="https://o-desafio-simon.vercel.app/" target="_blank">Acessar</a>
-                                </button>
-                                <button><a href="https://github.com/EzequielSR/O_Desafio_Simon"
-                                           target="_blank">Github</a>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <img src="/assets/Produto-API.png" alt="Descrição da imagem"/>
-                        <div className="card-about">
-                            <h2>Produto API - Testes de Integração</h2>
-                            <span>JAVA, SpringBoot, JUnity, MockMvc</span>
-                            <p>Produto API - Testes de Integração é uma aplicação que valida os endpoints de uma API de
-                                gerenciamento de produtos, utilizando Spring Boot, JUnit 5 e MockMvc para garantir o
-                                funcionamento correto das funcionalidades, como criação e listagem de produtos, através
-                                de
-                                testes automatizados.</p>
-                            <div className="buttons">
-                                <button><a href="https://github.com/EzequielSR/Produto-API_Testes_Integracao"
-                                           target="_blank">Github</a>
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card">
-                        <img src="/assets/ip-address-tracker.png" alt="Descrição da imagem"/>
-                        <div className="card-about">
-                            <h2>IP Address Tracker</h2>
-                            <span>React, JavaScript, Node.js, Express, LeafletJS, Axios</span>
-                            <p>IP Address Tracker é uma aplicação web que permite a pesquisa e visualização de
-                                informações de IP
-                                e domínios, mostrando dados como localização, fuso horário e ISP, além de exibir as
-                                coordenadas
-                                em um mapa interativo. Utiliza React no frontend, Node.js e Express no backend, com
-                                integração à
-                                API IPify.</p>
-                            <div className="buttons">
-                                <button><a href="https://ip-address-tracker-master-virid-nine.vercel.app/"
-                                           target="_blank">Acessar</a>
-                                </button>
-                                <button><a href="https://github.com/EzequielSR/ip-address-tracker-master"
-                                           target="_blank">Github</a></button>
-                            </div>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </section>
         </main>
-    )
+    );
 }
 
 export default ProjetosCard;
